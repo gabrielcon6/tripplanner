b64f34710144b27806629570dc98d59f23d8b3ca
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-10-22 20:46:22 +1100

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-    # Before any action is performed, call the fetch_user method.
+  # Before any action is performed, call the fetch_user method.
   before_action :fetch_user
-  ​
+
   private
-  ​
+
   def fetch_user
     # Search for a user by their user id if we can find one in the session hash.
     if session[:user_id].present?
-      @current_user = User.find_by :id => session[:user_id]
+      @current_user = User.find_by id: session[:user_id]
       # Clear out the session user_id if no user is found.
       session[:user_id] = nil unless @current_user
     end
