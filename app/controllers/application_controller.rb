d10b64ab1e8119fff3692e016eccd15099391426
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-10-22 20:39:37 +1100

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
     # Before any action is performed, call the fetch_user method.
-    before_action :fetch_user
+  before_action :fetch_user
   ​
-    private
+  private
   ​
-    def fetch_user
-      # Search for a user by their user id if we can find one in the session hash.
-      if session[:user_id].present?
-        @current_user = User.find_by :id => session[:user_id]
-        # Clear out the session user_id if no user is found.
-        session[:user_id] = nil unless @current_user
-      end
-    end
-  ​
-    def authorize_user
-      redirect_to '/login' unless @current_user.present?
+  def fetch_user
+    # Search for a user by their user id if we can find one in the session hash.
+    if session[:user_id].present?
+      @current_user = User.find_by :id => session[:user_id]
+      # Clear out the session user_id if no user is found.
+      session[:user_id] = nil unless @current_user
     end
   end
+  
+  def authorize_user
+    redirect_to '/login' unless @current_user.present?
+  end
+end
