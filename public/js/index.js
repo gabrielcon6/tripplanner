0380fe668bf180102d736d0874d5ddafb5e7dc32
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-11-01 11:54:53 +1100

    more stylying

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,6 @@
 
 const settingUpPages = function () {
     $('.calendar-body').hide();
-    $('.show-calendar').on('click', function () {
-        $('.calendar-body').slideDown('slow');
-    })
 };
 
 $(document).ready(settingUpPages);
